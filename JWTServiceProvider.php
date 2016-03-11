f92b565f38fc56448047d55b4d72d8c7b0561243
Pásztor Gábor <gpasztor87@gmail.com>
2016-03-11 11:41:21 +0100

    Initial commit

---
+++ JWTServiceProvider.php
@@ -0,0 +1,30 @@
+<?php namespace Autumn\JWTAuth;
+
+use Illuminate\Support\ServiceProvider;
+
+class JWTServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerMiddleware('jwt.auth', 'Tymon\JWTAuth\Middleware\GetUserFromToken');
+        $this->registerMiddleware('jwt.refresh', 'Tymon\JWTAuth\Middleware\RefreshToken');
+    }
+
+    /**
+     * Helper method to quickly setup middleware.
+     *
+     * @param string $alias
+     * @param string $class
+     */
+    protected function registerMiddleware($alias, $class)
+    {
+        $this->app['router']->middleware($alias, $class);
+    }
+
+}
---
+++ Plugin.php
@@ -0,0 +1,46 @@
+<?php namespace Autumn\JWTAuth;
+
+use App;
+use System\Classes\PluginBase;
+use Illuminate\Foundation\AliasLoader;
+
+/**
+ * JWTAuth Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * @var array Plugin dependencies
+     */
+    public $require = ['Autumn.Api', 'RainLab.User'];
+
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'JWTAuth',
+            'description' => 'JSON Web Token Authentication.',
+            'author'      => 'Autumn',
+            'icon'        => 'icon-user-secret'
+        ];
+    }
+
+    /**
+     * Register method, called when the plugin is first registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $alias = AliasLoader::getInstance();
+        $alias->alias('JWTAuth', 'Tymon\JWTAuth\Facades\JWTAuth');
+
+        App::register('Tymon\JWTAuth\Providers\JWTAuthServiceProvider');
+        App::register('Autumn\JWTAuth\JWTServiceProvider');
+    }
+
+}
