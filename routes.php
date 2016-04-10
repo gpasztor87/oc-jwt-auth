a72560ad15cd9128a26c18e93b604aadee641e40
Pásztor Gábor <gpasztor87@gmail.com>
2016-04-10 16:46:05 +0200

    Minor

--- routes.php
+++ routes.php
@@ -2,7 +2,7 @@
 
 Route::group(['prefix' => 'api/v1'], function() {
 
-    Route::post('auth/login', ' Autumn\JWTAuth\Http\Controllers\AuthController@authenticate');
-    Route::post('auth/register', ' Autumn\JWTAuth\Http\Controllers\AuthController@register');
+    Route::post('auth/login', 'Autumn\JWTAuth\Http\Controllers\AuthController@authenticate');
+    Route::post('auth/register', 'Autumn\JWTAuth\Http\Controllers\AuthController@register');
 
 });
