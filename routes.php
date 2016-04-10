f1b1f935a80fb1f58663d6c06804c8579e046964
Pásztor Gábor <gpasztor87@gmail.com>
2016-04-10 16:42:39 +0200

    Minor fix

--- routes.php
+++ routes.php
@@ -2,7 +2,7 @@
 
 Route::group(['prefix' => 'api/v1'], function() {
 
-    Route::post('auth/login', ' Autumn\JWTAuth\Http\ControllersAuthController@authenticate');
-    Route::post('auth/register', ' Autumn\JWTAuth\Http\ControllersAuthController@register');
+    Route::post('auth/login', ' Autumn\JWTAuth\Http\Controllers\AuthController@authenticate');
+    Route::post('auth/register', ' Autumn\JWTAuth\Http\Controllers\AuthController@register');
 
 });
