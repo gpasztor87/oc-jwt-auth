856338d40f8f6ee16cf245aa4fc7c424f9390c3d
Pásztor Gábor <gpasztor87@gmail.com>
2017-03-01 21:48:26 +0100

    StyleCI

--- routes.php
+++ routes.php
@@ -7,7 +7,7 @@ $api->version('v1', function ($api) {
     $api->post('auth/register', 'Autumn\JWTAuth\Http\Controllers\AuthController@register');
     $api->post('auth/logout', 'Autumn\JWTAuth\Http\Controllers\AuthController@logout');
 
-    $api->group(['middleware' => 'jwt.auth'], function($api) {
+    $api->group(['middleware' => 'jwt.auth'], function ($api) {
         $api->get('auth/me', 'Autumn\JWTAuth\Http\Controllers\AuthController@user');
     });
 });
